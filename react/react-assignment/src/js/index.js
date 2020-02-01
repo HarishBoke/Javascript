045d52f4cd30b4bb93ddcdd0f1726e02bb3b828e
= <harish.boke@gmail.com>
2020-02-01 22:56:34 +0530

    reset repo

--- react/react-assignment/src/js/index.js
+++ react/react-assignment/src/js/index.js
@@ -1,10 +1,10 @@
 import store from '../js/store/index';
 
-import  { addArticle, setCounter } from '../js/actions/index';
+import  { addArticle } from '../js/actions/index';
 
 window.store = store;
 window.addArticle = addArticle;
-window.setCounter = setCounter;
+//window.setCounter = setCounter; 
 
 console.log(`store.getState() ${JSON.stringify(store.getState())}`); 
 
