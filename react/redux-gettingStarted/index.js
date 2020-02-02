3d93d067338ecd36c9863b412af1626b5c9beb5b
= <harish.boke@gmail.com>
2020-02-02 22:36:23 +0530

    learn: redux, basics in single file. #gettingStarted

---
+++ react/redux-gettingStarted/index.js
@@ -0,0 +1,53 @@
+
+//Node import
+
+const redux = require('redux')
+const createStore = redux.createStore
+
+// Constants
+const BUY_CAKE = 'BUY_CAKE'
+
+
+// Action
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: 'First Redux Action'
+    }
+}
+
+
+// Reducer 
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes -1
+        }
+    
+        default: return state
+    }
+}
+
+// Store
+
+const store = createStore(reducer)
+
+//getState()
+console.log('initial state', store.getState())
+
+// subscribe
+// catch subcribe method return so we can call it later 
+const unsubscibe = store.subscribe( () => console.log('Updated State:', store.getState()))
+
+
+//dispatch 
+store.dispatch(buyCake) //accepts action / action creator
+store.dispatch(buyCake)
+store.dispatch(buyCake)
+
+unsubscibe();
