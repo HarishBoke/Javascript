8648778c6e30bce136a75a0e25b374449a4a125b
= <harish.boke@gmail.com>
2020-04-17 13:15:21 +0530

    run server with node without webserver external tool

---
+++ runWebserver_bat_sh.sh
@@ -0,0 +1,9 @@
+npx browser-sync start --server --files "./*.html" --no-open --no-notify --directory
+
+# Run file :  sh runWebserver_bat_sh.sh
+# Copy and paste the start script in a terminal window to locally serve all the files. Access the files in localhost:3000.
+
+# npx browser-sync start --server --files "./*.html" --no-open --no-notify --directory
+# npx comes bundled with Node.js
+# browsersync automatically reloads any changes made to the code.
+# More info on browsersync
